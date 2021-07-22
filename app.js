5f20c40053c952b8d89feedf3a22603f46607297
jos_aa <jos_aaron@hotmail.com>
2021-07-22 14:53:04 -0600

    Menu interactivo

--- app.js
+++ app.js
@@ -1,2 +1,55 @@
 'use strict';
-console.log('hola mundo');
+
+const { 
+    leerInput,
+    inquirerMenu,
+    pausa
+} = require("./helpers/inquirer");
+const Busqueda = require("./models/Busquedas");
+
+const main = async() => {
+   
+    const busqueda = new Busqueda();
+    let opt = '';
+
+    do{
+      opt  =  await inquirerMenu();
+
+      switch(opt){
+        case 1:
+            //Mostrar mensaje
+            const lugar = await leerInput('Ciudad: '.yellow);
+            console.log(lugar);
+            //Buscar los lugares
+            busqueda.ciudad(lugar);
+            //seleccionar el lugar
+
+            //clima
+
+            //mostrar resultado
+            console.log('\n Información de la ciudad:\n'.green);
+            console.log('Ciudad:', );
+            console.log('Lat:', );
+            console.log('Lng:', );
+            console.log('Temperatura:', );
+            console.log('Mínima:', );
+            console.log('Máxima:', );
+
+
+            await pausa();
+        break;
+
+        case 2:
+
+        break;
+
+        case 3:
+        
+        break
+      }
+      
+      if(opt === 0) await pausa();
+
+    }while(opt !== 0)
+}
+main();
---
+++ models/busquedas.js
@@ -0,0 +1,21 @@
+'use strict';
+class Busqueda{
+    historial = ['Tegucigalpa', 'Madrid', 'San José'];
+    
+    constructor(){
+        //TODO: leer DB si existe.
+    }
+
+    async ciudad( lugar = '' ){
+        //Peticion http.
+        console.log('lugar');
+
+        return [] //retorna los lugares
+    }
+
+
+
+
+}
+
+module.exports = Busqueda;
